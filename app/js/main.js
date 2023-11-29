8a6640d75e3ecd782531a130a1c2e78f2b9cce67
phildan02 <phildan02@gmail.com>
2023-11-29 12:00:53 +0300

    slider fixed

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,44 @@
+$('.slider__inner').on('init', function(...args){
+    const slideCount = args[1].slideCount;
+    const currentSlide = args[1].currentSlide + 1;
+    $('.slider__total-number').html(slideCount);
+    $('.slider__current-number').html(currentSlide);
+});
+
 $('.slider__inner').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    speed: 1000,
+    speed: 3000,
     dots: true,
+    // autoplay: true,
+    // autoplaySpeed: 1000,
+    pauseOnDotsHover: true,
     appendDots: $('.slider__counter'),
     prevArrow: $('.slider__arrow-left'),
     nextArrow: $('.slider__arrow-right'),
 });
 
-function setSliderCounter() {
-    let curSliderPos = $('.slick-active button').attr('aria-label').split('of').map((item) => item.trim());
-    $('.slider__current-number').html(curSliderPos[0]);
-    $('.slider__total-number').html(curSliderPos[1]);
-}
-setSliderCounter();
+$('.slider__inner').on('afterChange', function(...args){
+    const currentSlide = args[2] + 1;
+    $('.slider__current-number').html(currentSlide);
+});
 
-$('.slider__arrow-left').on('click', setSliderCounter);
-$('.slider__arrow-right').on('click', setSliderCounter);
+function slickPause() {
+    $('.slider__inner').slick('slickPause');
+}
+function slickPlay() {
+    $('.slider__inner').slick('slickPlay');
+}
+
+$('.slider__menu').on('pointerover', () => {
+    slickPause();
+});
+$('.slider__menu').on('pointerout', () => {
+    slickPlay();
+});
+$('.slick-arrow').on('focus', () => {
+    slickPause();
+});
+$('.slick-arrow').on('blur', () => {
+    slickPlay();
+});
